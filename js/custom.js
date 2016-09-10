3c8026699ac5f254e6dea7a96e08e02cb9c50e1f
justapps4all <ghost@gitbook.com>
2016-09-10 02:07:12 +0200

    Update js/custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,9 @@
-/*
+
 $(function() {
-  setInterval(function(){ 
-    $('li.chapter').off();
-    $('li.chapter').on('click',function(){
-      $(this).addClass('myclass1');
-      $(this).parent().addClass('myclass2');
-    });
-  }, 1000);
+
+  $('.custom-link').attr('href','');
+
+/*
 
   setTimeout(function(){
   
@@ -18,8 +15,10 @@ $(function() {
       $(this).parent().css('display','block');
     });
   },1000);
+  
+  */
 });
-*/
+
 
 
 
